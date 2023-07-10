9c771a8d6096eff8427adc28dc8d0434f32b24e2
Mehmet Ali Yılmaz <mehmetaliyilmaz2741@outlook.com>
2023-07-10 03:06:04 +0300

    daire alan ve cevresini hesaplayan kodlar yazildi

---
+++ daire.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class daire {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int r;
+        double pi = 3.14;
+        System.out.print("Dairenin Yarı Çapını Giriniz: ");
+        r = input.nextInt();
+        double alan = pi * r * r;
+        double cevre = 2 * pi * r;
+
+        System.out.println("Dairenin Alanı: " + alan);
+        System.out.println("Dairenin Cevresi: " + cevre);
+
+    }
+
+}
